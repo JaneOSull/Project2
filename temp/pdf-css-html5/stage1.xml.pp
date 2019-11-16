ac41b912dadf561c9eb2968f5fbe27eea2ef8a71
JaneOSull <jane.osullivan@mycit.ie>
2019-11-16 20:49:26 +0000

    my second commit

---
+++ temp/pdf-css-html5/stage1.xml.pp
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="UTF-8"?><map xmlns:ditaarch="http://dita.oasis-open.org/architecture/2005/" xmlns:opentopic-index="http://www.idiominc.com/opentopic/index" xmlns:xs="http://www.w3.org/2001/XMLSchema" xmlns:oxy="http://www.oxygenxml.com/extensions/author" xmlns:nd="http://www.oxygenxml.com/css2fo/named-destinations" xmlns:opentopic="http://www.idiominc.com/opentopic" xmlns:css2fo="http://www.oxygenxml.com/css2fo" xmlns:dita-ot="dita-ot.sourceforge.net/ns/201007/dita-ot" cascade="merge" class="- map/map " ditaarch:DITAArchVersion="1.3" domains="(map mapgroup-d) (topic abbrev-d) (topic delay-d) a(props deliveryTarget) (map ditavalref-d) (map glossref-d) (topic hazard-d) (topic hi-d) (topic indexing-d) (topic markup-d) (topic pr-d) (topic relmgmt-d) (topic sw-d) (topic ui-d) (topic ut-d) (topic markup-d xml-d)" xtrc="map:1;3:6" xtrf="file:/C:/Users/Jane/Documents/Project2/Project2.ditamap" xml:lang="en"><oxy:front-page class="- front-page/front-page "><oxy:front-page-title class="- front-page/front-page-title "><title xmlns:dita-ot="http://dita-ot.sourceforge.net/ns/201007/dita-ot" xmlns:ot-placeholder="http://suite-sol.com/namespaces/ot-placeholder" class="- topic/title "/></oxy:front-page-title></oxy:front-page><opentopic:map xmlns:ot-placeholder="http://suite-sol.com/namespaces/ot-placeholder" class="- toc/toc "><oxy:toc-title class="- toc/title " empty="true"/>
+    <title xmlns:dita-ot="http://dita-ot.sourceforge.net/ns/201007/dita-ot" class="- topic/title "/>
+    <topicref xmlns:dita-ot="http://dita-ot.sourceforge.net/ns/201007/dita-ot" is-chapter="true" class="- map/topicref " href="#unique_1" type="concept"><topicmeta class="- map/topicmeta " data-topic-id="about"><resourceid appid="about" class="- topic/resourceid " oxy-source="topic"/><navtitle href="#unique_1" class="- topic/navtitle ">About this publication</navtitle><?ditaot gentext?><?ditaot genshortdesc?><shortdesc class="- map/shortdesc "/></topicmeta></topicref>
+    <topicref xmlns:dita-ot="http://dita-ot.sourceforge.net/ns/201007/dita-ot" is-chapter="true" class="- map/topicref " href="#unique_2" type="concept"><topicmeta class="- map/topicmeta " data-topic-id="SolutionComponents"><resourceid appid="SolutionComponents" class="- topic/resourceid " oxy-source="topic"/><navtitle href="#unique_2" class="- topic/navtitle ">IBM StoredIQ components</navtitle><?ditaot gentext?><?ditaot genshortdesc?><shortdesc class="- map/shortdesc ">The IBM StoredIQ solution consists of these components: the application stack, the gateway, the data         server, and optionally the Elasticsearch cluster.</shortdesc></topicmeta></topicref>
+</opentopic:map><concept xmlns:dita-ot="http://dita-ot.sourceforge.net/ns/201007/dita-ot" break-before="true" is-chapter="true" class="- topic/topic concept/concept " id="unique_1" topicrefclass="- map/topicref " nd:nd-id="about" oid="about">
+    <title class="- topic/title ">About this publication</title>
+    <shortdesc class="- topic/shortdesc "/>
+    <conbody class="- topic/body  concept/conbody ">
+        <p class="- topic/p "><i class="+ topic/ph hi-d/i ">IBM StoredIQ Deployment and Configuration Guide</i> provides information about how to plan, deploy, and
+            configure the IBM StoredIQ product.</p>
+        <section class="- topic/section ">
+            <title class="- topic/title ">IBM StoredIQ product library</title>
+            <p class="- topic/p ">The following documents are available in the IBM® StoredIQ® product library.</p>
+            <ul class="- topic/ul ">
+                <li class="- topic/li "><i class="+ topic/ph hi-d/i ">IBM StoredIQ Overview Guide</i></li>
+                <li class="- topic/li "><i class="+ topic/ph hi-d/i ">IBM StoredIQ Deployment and Configuration Guide</i></li>
+                <li class="- topic/li "><i class="+ topic/ph hi-d/i ">IBM StoredIQ Data Server Administration Guide</i></li>
+                <li class="- topic/li "><i class="+ topic/ph hi-d/i ">IBM StoredIQ Administrator Administration Guide</i></li>
+                <li class="- topic/li "><i class="+ topic/ph hi-d/i ">IBM StoredIQ Data Workbench User Guide</i></li>
+                <li class="- topic/li "><i class="+ topic/ph hi-d/i ">IBM StoredIQ Policy Manager User Guide</i></li>
+                <li class="- topic/li "><i class="+ topic/ph hi-d/i ">IBM StoredIQ Insights User Guide</i></li>
+                <li class="- topic/li "><i class="+ topic/ph hi-d/i ">IBM StoredIQ Integration Guide</i></li>
+            </ul>
+        </section>
+    </conbody>
+</concept><concept xmlns:dita-ot="http://dita-ot.sourceforge.net/ns/201007/dita-ot" break-before="true" is-chapter="true" class="- topic/topic concept/concept " id="unique_2" topicrefclass="- map/topicref " nd:nd-id="SolutionComponents" oid="SolutionComponents">
+    <title class="- topic/title ">IBM StoredIQ components</title>
+    <shortdesc class="- topic/shortdesc ">The IBM StoredIQ solution consists of these components: the application stack, the gateway, the data
+        server, and optionally the Elasticsearch cluster.</shortdesc>
+         <conbody class="- topic/body  concept/conbody "> 
+        <section class="- topic/section ">
+            <title class="- topic/title ">Solution components</title>
+            <p class="- topic/p ">IBM StoredIQ provides three solution components: the gateway, data servers, and application stack
+                (AppStack)</p>
+            
+        </section>
+             <section class="- topic/section ">
+                 <title class="- topic/title ">Applications of IBM StoredIQ</title>
+                 <p class="- topic/p ">IBM StoredIQ provides interface applications that help fulfill its solution goals.</p>
+             </section>
+             <section class="- topic/section ">
+                 <title class="- topic/title ">IBM StoredIQ Data Server</title>
+                 <p class="- topic/p ">IBM StoredIQ Data Server user interface provides access to data server functionality. It allows
+                     administrators to view the dashboard and see the status of the jobs and system details. Administrators
+                     can manage information about servers and conduct various configurations on the system and application
+                     settings.</p>
+                 <image class="- topic/image " href="file:/Users/Jane/Documents/Project2/resources/images/dashboard.JPG" placement="inline"/>
+             </section>
+         </conbody>
+    
+</concept></map>
